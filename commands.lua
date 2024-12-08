76dff8a8d06c8ced38e9304f8b2faed2b59b0430
Nxrdo <61379446+Nxrdo@users.noreply.github.com>
2024-12-07 22:42:02 -0600

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -1,48 +1,83 @@
-local api = {}
-local methods = {}
-
-local function startsWith(s, v)
-	return s:sub(1, #v) == v
+local function startsWith(str, start)
+	return str:sub(1, #start) == start
 end
 
-function api.New(prefix)
-	return setmetatable({
+local function assertf(condition, fmessage, ...)
+	return assert(condition, string.format(fmessage, ...))
+end
+
+local function newInstance()
+	local commands = {
+		prefix = '/',
+		aliases = {},
 		commands = {},
-		prefix = prefix or '/'
-	}, { __index = methods })
-end
-
-function methods:AddCommand(name, alias, callback)
-	if not callback then
-		callback, alias = alias, callback
-	end
+	}
 	
-	if type(alias) == 'table' then
-		for _, command in ipairs(alias) do
-			self.commands[command] = callback
+	function commands.create(cfg)
+		local name = cfg.name
+		local desc = cfg.desc or 'No description'
+		local places = cfg.places or {}
+		local aliases = cfg.aliases or {}
+		local callback = cfg.callback
+
+		assert(name and name:match('^%s*$') == nil, 'field "name" not found or is empty')
+		assert(callback or type(callback) == 'function', 'field "callback" not provided or is not a function')
+
+		local foundAlias = commands.aliases[name]
+		local foundCommand = commands.commands[name]
+
+		assertf(
+			(foundAlias or foundCommand) == nil,
+			'command %q conflicts with an existing %s', name, foundAlias and 'alias' or 'command')
+
+		commands.commands[name] = {
+			desc = desc,
+			places = places,
+			aliases = {},
+			callback = callback
+		}
+
+		for _, alias in ipairs(aliases) do
+			assertf(
+				(commands.commands[alias] or commands.aliases[alias]) == nil,
+				'alias %q conflicts with an existing %s',alias, commands.aliases[alias] and 'alias' or 'command')
+
+			table.insert(commands.commands[name].aliases, alias)
+			commands.aliases[alias] = name
 		end
 	end
-	
-	self.commands[name] = type(callback) == 'function' and callback or alias
-end
 
-function methods:ParseCommand(speaker, input, prefix)
-	prefix = prefix or self.prefix
-	
-	if not startsWith(input, prefix) then
-		return false, 'Invalid prefix'
+	function commands.parse(message, enforcePrefix)
+		local args = {}
+
+		for word in message:gmatch('[%w%p]+') do
+			table.insert(args, word)
+		end
+
+		if not args[1] then return true end
+
+		local command = args[1]:lower()
+		local hasPrefix = startsWith(command, commands.prefix)
+		command = hasPrefix and command:sub(#commands.prefix + 1) or command
+
+		if not hasPrefix and enforcePrefix then
+			return false, 'prefix not found in message'
+		end
+
+		local found = commands.commands[commands.aliases[command] or command]
+
+		if not found then
+			return false, string.format('command %q does not exist', command)
+		end
+
+		if #found.places > 0 and not table.find(found.places, game.PlaceId) then
+			return false, string.format('command %q is not available in this place', command)
+		end
+
+		return pcall(found.callback, (table.unpack or unpack)(args, 2))
 	end
 	
-	local split = input:split(' ')
-	local command = split[1]:sub(#prefix + 1)
-	
-	if not self.commands[command] then
-		return false, string.format('command \'%s\' does not exist', command)
-	end
-	
-	local args = {table.unpack(split, 2)}
-	
-	return true, self.commands[command](args, speaker)
+	return commands
 end
 
-return api
+return newInstance()
